1cb1ffb131c7cdfef68c2d1bf202ced7306e2ed8
TensorEleven <harivonyratefiarison@gmail.com>
2022-03-14 11:21:17 +0300

    ADD linimum setup to understand middle ware

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const { send } = require('process')
+
+const app = express()
+
+// Global Middleware running action
+app.use(logger)
+
+// Routing
+app.get('/',(req,res)=>{
+    console.log("Home page")
+    res.send('Home page')
+})
+
+app.get('/users', auth ,(req,res,next)=>{
+    console.log("users page")
+    res.send('Users page')
+    next();
+})
+
+
+// Middle-ware Definition
+function logger(req,res,next){
+    console.log("Log")
+    next()
+}
+
+function auth(req,res,next){
+    if(req.query.admin ==="true"){
+        console.log("[ok] Auth")
+        next()
+    }
+    else{
+        res.send('Access denied')
+        console.log("[not ok] Auth")
+    }
+}
+
+
+// Start server
+app.listen(3000,()=>console.log("Server started"))
